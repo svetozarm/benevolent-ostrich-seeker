5d981449e269069c1e2335c27ad1117fd8ff8f68
Svetozar Miucin <smiucin@sfu.ca>
2015-08-31 00:06:32 +0000

    apply_patches.sh extended to support tidy

--- speccfg/apply_patches.sh
+++ speccfg/apply_patches.sh
@@ -2,4 +2,19 @@
 
 PATCH_DIR=`pwd`
 cd $SPEC_INSTALL/benchspec
-patch -p0 < $PATCH_DIR/makefile_linkage.patch
+
+if [ "$1" = "tidy" ]
+then
+    echo "Setting up patches for tidy"
+    patch -p0 -R < $PATCH_DIR/makefile_llvmtrack.patch
+    patch -p0 < $PATCH_DIR/makefile_tidy.patch
+fi
+
+if [ "$1" = "llvmtrack" ]
+then
+    echo "Setting up patches for llvmtrack"
+    patch -p0 -R < $PATCH_DIR/makefile_tidy.patch
+    patch -p0 < $PATCH_DIR/makefile_llvmtrack.patch
+fi
+
+
