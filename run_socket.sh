e3dbe5099084e88dfacf989d6a128d7abf47a12d
Svetozar Miucin <smiucin@sfu.ca>
2015-10-12 23:27:19 +0000

    fixes, plus a new alloc-config

--- run_socket.sh
+++ run_socket.sh
@@ -2,7 +2,7 @@
 
 : ${SPEC_INSTALL:?"Need to set SPEC_INSTALL to point to your SPECCPU2006 installation dir"}
 : ${INST_LIB:?"Need to set INST_LIB to point to the directory where your libinstrumentation.so and instrumentation.bc are stored"}
-: ${EXTRACTOR_DIR}:?"Need to set EXTRACTOR_DIR to point to the socket_extractor directory"
+: ${EXTRACTOR_DIR:?"Need to set EXTRACTOR_DIR to point to the socket_extractor directory"}
 
 CONFDIR=`readlink -f ./alloc-configs`
 
@@ -58,7 +58,7 @@ do
     cp $SPEC_INSTALL/benchspec/CPU2006/$BENCH_NAME/run/run*0000/access.trace $RESULT_DIR/$BENCH_NAME.trace
     cp $SPEC_INSTALL/benchspec/CPU2006/$BENCH_NAME/run/run*0000/trace.bin $RESULT_DIR
     cp $SPEC_INSTALL/benchspec/CPU2006/$BENCH_NAME/build/build*0000/map*.json $RESULT_DIR
-    $CURRENT/extract_code.py $SPEC_INSTALL/benchspec/CPU2006/$BENCH_NAME/src $RESULT_DIR $RESULT_DIR/extracted/
+    $CURRENT/extract_code.py $SPEC_INSTALL/benchspec/CPU2006/$BENCH_NAME/src $RESULT_DIR $RESULT_DIR/extracted/ > $RESULT_DIR/code.txt
 done
 
 
